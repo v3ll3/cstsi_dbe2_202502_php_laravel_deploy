e69f3e15d1c2764cb5106a15a115d5ce768ef107
V3ll3 <gillgonzales.pf358@academico.ifsul.edu.br>
2026-06-12 22:42:24 -0300

    deploy: configs deploy azure

--- topico02/resources/js/routes/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-import { Navigate } from "react-router";
-import { useAuthContext } from "../contexts/AuthProvider";
-
-export default function PrivateRoute({ children }){
-  const { user } = useAuthContext();
-  return user ? children : <Navigate to="/login" />;
-};
