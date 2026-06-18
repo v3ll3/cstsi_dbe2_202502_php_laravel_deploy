09219eb4684f00dc2b313713035a964dabe860c2
V3ll3 <gillgonzales.pf358@academico.ifsul.edu.br>
2026-06-18 19:54:43 -0300

    fix: vite build envs

--- resources/js/utils/axios-client.js
+++ resources/js/utils/axios-client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const SERVER_URL = import.meta.env.VITE_API_HOST
 export const BASE_URL =  import.meta.env.VITE_API_URL
-console.log({BASE_URL});
+console.log({BASE_URL,SERVER_URL});
 
 const axiosClient = axios.create({
     baseURL: BASE_URL,
