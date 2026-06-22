406e065cbf43df7a103b2a0ddec55f3b225fd76c
V3ll3 <gillgonzales.pf358@academico.ifsul.edu.br>
2026-06-22 08:51:19 -0300

    chore: config cors allow all fronts url

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(';',env('FRONTEND_URL','*')),
+    // 'allowed_origins' => explode(';',env('FRONTEND_URL','*')),
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
